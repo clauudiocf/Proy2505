e5550864cf438d2eee3ceafd631aecc89d4396e0
cetecom <cetecom@LC08L1204>
2017-05-25 13:20:59 -0400

    23-05

--- dato/Usuario.php
+++ dato/Usuario.php
@@ -45,4 +45,21 @@ class Usuario{
         
     }
     
+    function ModificarClave(){
+                $oConn=new Conexion();
+        
+        if ($oConn->Conectar())
+            $db=$oConn->objconn;
+        else
+            return false;
+        
+        $clavemd5=md5($this->clave);
+        $sql="UPDATE * FROM acceso WHERE nomusuario='$this->nombre' and pwdusuario='$clavemd5'";
+          $resultado=$db->query($sql);
+          if ($resultado->num_rows>=1)
+            return true;
+        else
+            return false;
+    }
+    
 }
